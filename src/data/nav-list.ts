8ec07483bce89556dd363df60858e348167b3b92
arifwp <arifwahyu696@gmail.com>
2024-05-20 16:54:44 +0700

    add data

---
+++ src/data/nav-list.ts
@@ -0,0 +1,18 @@
+export const navs = [
+    {
+      link: "#homeSection",
+      label: "Home",
+    },
+    {
+      link: "#aboutSection",
+      label: "Tentang",
+    },
+    {
+      link: "#eventsSection",
+      label: "Event",
+    },
+    {
+      link: "#contactSection",
+      label: "Kontak",
+    },
+  ];
---
+++ src/data/sponsors.ts
@@ -0,0 +1,37 @@
+export const sponsors = [
+    {
+      imgUrl: '/images/sponsors/spotify.png',
+      name: "Spotify",
+      isBig: true,
+    },
+    {
+      imgUrl: '/images/sponsors/tokped.png',
+      name: "Tokopedia",
+      isBig: true,
+  },
+  {
+    imgUrl: '/images/sponsors/audi.png',
+    name: "Audi",
+    isBig: false,
+  },
+  {
+    imgUrl: '/images/sponsors/citibank.png',
+    name: "Citibank",
+    isBig: false,
+  },
+  {
+    imgUrl: '/images/sponsors/cocacola.png',
+    name: "Coca Cola",
+    isBig: true,
+  },
+  {
+    imgUrl: '/images/sponsors/coffe.png',
+    name: "Coffe Weekdays",
+    isBig: false,
+  },
+  {
+    imgUrl: '/images/sponsors/dairyqueen.png',
+    name: "Dairy Queen",
+    isBig: false,
+    },
+  ];
