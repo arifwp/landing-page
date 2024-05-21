6be99d0bca50105941a88c76c571e7ceb74c2a5a
arifwp <arifwahyu696@gmail.com>
2024-05-21 17:03:05 +0700

    add drawer

--- src/components/independent/Navbar.tsx
+++ src/components/independent/Navbar.tsx
@@ -1,11 +1,30 @@
-import { Box, Button, HStack, Icon, IconButton, Text } from "@chakra-ui/react";
-import { HashLink } from "react-router-hash-link";
-import useScreenWidth from "../../lib/useScreenWidth";
-import { navs } from "../../data/nav-list";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  HStack,
+  Icon,
+  IconButton,
+  Image,
+  Text,
+  VStack,
+  Wrap,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { RiMenu4Line } from "@remixicon/react";
+import { HashLink } from "react-router-hash-link";
+import { event } from "../../data/event";
+import { navs } from "../../data/nav-list";
+import useScreenWidth from "../../lib/useScreenWidth";
 
 const Navbar = () => {
   const sw = useScreenWidth();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <HStack
@@ -36,10 +55,52 @@ const Navbar = () => {
       )}
 
       {sw < 768 && (
-        <IconButton
-          aria-label="nav-button"
-          icon={<Icon as={RiMenu4Line} fontSize={20} />}
-        />
+        <>
+          <IconButton
+            aria-label="nav-button"
+            icon={<Icon as={RiMenu4Line} fontSize={20} onClick={onOpen} />}
+          />
+          <Drawer onClose={onClose} isOpen={isOpen} size={"xs"}>
+            <DrawerOverlay />
+            <DrawerContent bg={"#E53E3E"}>
+              <DrawerCloseButton />
+              <DrawerHeader>
+                <VStack>
+                  <Image src={event.eventLogo} h="80px" />
+                  <Text color="white">{event.eventName}</Text>
+                </VStack>
+              </DrawerHeader>
+              <DrawerBody>
+                <VStack alignItems="left">
+                  <Wrap justify="center">
+                    <Box bg="white" h="1px" w="90%" opacity="0.3"></Box>
+                  </Wrap>
+                  {navs.map((nav, i) => (
+                    <HashLink to={nav.link} key={i}>
+                      <Button
+                        key={i}
+                        colorScheme="red"
+                        textColor="white"
+                        variant="ghost"
+                      >
+                        {nav.label}
+                      </Button>
+                    </HashLink>
+                  ))}
+                </VStack>
+              </DrawerBody>
+              <Box position="relative" opacity="0.1">
+                <Image
+                  src={event.eventLogo}
+                  h="200px"
+                  position="absolute"
+                  bottom="0"
+                  right="0"
+                />
+              </Box>
+            </DrawerContent>
+          </Drawer>
+        </>
       )}
     </HStack>
   );
