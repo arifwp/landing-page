f02e03d26ba2c623ad6d5e55132af844364305d6
arifwp <arifwahyu696@gmail.com>
2024-05-21 17:02:49 +0700

    add contact section

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import HomeSection from "../sections/Home";
+import AboutSection from "../sections/About";
+import ScheduleSection from "../sections/Schedule";
+import ContactSection from "../sections/Contact";
+
+export default function Landing() {
+  return (
+    <>
+      <HomeSection />
+
+      <AboutSection />
+
+      <ScheduleSection />
+
+      <ContactSection />
+    </>
+  );
+}
