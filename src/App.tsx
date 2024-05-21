0a4dc48eb8b0e66bc3a4d2ae429c3fae4ca65b53
arifwp <arifwahyu696@gmail.com>
2024-05-21 17:03:24 +0700

    change route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import Landing from "./sections/Landing";
+import Landing from "./pages/Landing";
 import Navbar from "./components/independent/Navbar";
 
 export const App = () => (
