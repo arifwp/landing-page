52546bf2c6352d3ce457cdbcf72e0f6dda013e84
arifwp <arifwahyu696@gmail.com>
2024-05-30 15:14:09 +0700

    remove unused function

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,32 +1,5 @@
 import FileSaver from "file-saver";
 
-export const getImgHeight = (imageUrl: string) => {
-    const img = new Image();
-    img.src = imageUrl;
-
-    let newheight: number;
-    newheight = 0;
-    if (img.height > 1500) {
-        newheight = img.height / 2;
-    }
-
-    return newheight;
-}
-
-export const getImgWidth = (imageUrl: string) => {
-    const img = new Image();
-    img.src = imageUrl;
-
-    let newWidth: number;
-    newWidth = 0;
-
-    if (img.width > 1500) {
-        newWidth = img.width / 2;
-    }
-
-    return newWidth;
-}
-
 export const downloadImage = (src: string) => {
-    FileSaver.saveAs(`http://localhost:3000${src}`, 'KETAON FAIR.jpg');
-}
+  FileSaver.saveAs(`http://localhost:3000${src}`, "KETAON FAIR.jpg");
+};
