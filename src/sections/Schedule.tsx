72832c1af9f30c2528bbc47a057f8f0814d13b05
arifwp <arifwahyu696@gmail.com>
2024-05-30 15:13:34 +0700

    change layout

--- src/sections/Schedule.tsx
+++ src/sections/Schedule.tsx
@@ -1,27 +1,138 @@
-import { Heading, Text, VStack, Wrap } from "@chakra-ui/react";
+import { HStack, Stack, Text, VStack, Wrap, WrapItem } from "@chakra-ui/react";
+import Empty from "../components/Empty";
 import Event from "../components/Event";
 import { eventList } from "../data/event-list";
+import ScheduleItem from "../components/ScheduleItem";
 
 const ScheduleSection = () => {
   return (
     <section id="scheduleSection">
-      <VStack bg={"black"} px={8} py={24} boxSize={"cover"}>
-        <VStack>
-          <Text color={"blue.500"}>lorem ipsum dolor sit amet</Text>
-          <Heading color={"white"} textAlign={"center"}>
-            Lorem ipsum dolor sit amet gila saja
-          </Heading>
-          <Text color={"white"}>ini description</Text>
-          <Wrap spacing={"30px"} justify={"center"} mt={10} mx={"auto"}>
+      <VStack
+        backgroundImage="url('/images/bg_artist.png')"
+        backgroundPosition="center"
+        backgroundSize="cover"
+        backgroundRepeat="no-repeat"
+        py={24}
+        boxSize={"cover"}
+      >
+        <Wrap py={4} spacing={10} align="center" justify="center" px={2}>
+          <WrapItem>
+            <Stack direction={["row", "row", "column", "column"]} align={"end"}>
+              <Text
+                color={"white"}
+                fontSize={{ base: "12px", md: "14px", lg: "16px" }}
+              >
+                ayo ramaikan
+              </Text>
+              <Text
+                color={"white"}
+                fontSize={{ base: "12px", md: "14px", lg: "16px" }}
+              >
+                datang ke
+              </Text>
+              <Text
+                color={"white"}
+                fontSize={{ base: "12px", md: "14px", lg: "16px" }}
+              >
+                event ini
+              </Text>
+            </Stack>
+          </WrapItem>
+
+          <WrapItem>
+            <HStack spacing={8} align={"stretch"}>
+              <VStack>
+                <Text
+                  color={"white"}
+                  fontWeight={"bold"}
+                  fontSize={{
+                    base: "20px",
+                    sm: "40px",
+                    md: "40px",
+                    lg: "68px",
+                  }}
+                >
+                  30
+                </Text>
+                <Text
+                  color={"blue.500"}
+                  fontWeight={"semibold"}
+                  fontSize={{ base: "12px", md: "16px", lg: "24px" }}
+                >
+                  Tanggal
+                </Text>
+              </VStack>
+
+              {/* <Heading color={"white"}>/</Heading> */}
+
+              <VStack>
+                <Text
+                  color={"white"}
+                  fontWeight={"bold"}
+                  fontSize={{
+                    base: "20px",
+                    sm: "40px",
+                    md: "40px",
+                    lg: "68px",
+                  }}
+                >
+                  September
+                </Text>
+                <Text
+                  color={"blue.500"}
+                  fontWeight={"semibold"}
+                  fontSize={{ base: "12px", md: "16px", lg: "24px" }}
+                >
+                  Bulan
+                </Text>
+              </VStack>
+
+              {/* <Heading color={"white"}>/</Heading> */}
+
+              <VStack>
+                <Text
+                  color={"white"}
+                  fontWeight={"bold"}
+                  fontSize={{
+                    base: "20px",
+                    sm: "40px",
+                    md: "40px",
+                    lg: "68px",
+                  }}
+                >
+                  2024
+                </Text>
+                <Text
+                  color={"blue.500"}
+                  fontWeight={"semibold"}
+                  fontSize={{ base: "12px", md: "16px", lg: "24px" }}
+                >
+                  Tahun
+                </Text>
+              </VStack>
+            </HStack>
+          </WrapItem>
+        </Wrap>
+
+        {eventList.length > 0 ? (
+          <Wrap spacing={"30px"} justify={"center"} mt={10} mx={"auto"} px={8}>
+            {eventList.map((event, i) => (
+              // <Event data={event} key={i} />
+
+              <ScheduleItem data={event} key={i} />
+            ))}
+          </Wrap>
+        ) : (
+          <Empty componentName="schedule" />
+        )}
+
+        {/* <Wrap spacing={"30px"} justify={"center"} mt={10} mx={"auto"}>
             {eventList.length > 0 ? (
               eventList.map((event, i) => <Event data={event} key={i} />)
             ) : (
-              <Text color={"white"}>
-                ~ Belum ada informasi untuk ditampilkan ~
-              </Text>
+              <Empty componentName="schedule" />
             )}
-          </Wrap>
-        </VStack>
+          </Wrap> */}
       </VStack>
     </section>
   );
