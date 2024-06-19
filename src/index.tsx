5e4db0791b6350134e813c2ba6ed702f0ac10d1c
arifwp <arifwahyu696@gmail.com>
2024-06-19 12:19:43 +0700

    add speedinsight and analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
@@ -8,6 +10,8 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <Analytics />
+    <SpeedInsights />
     <App />
   </React.StrictMode>
 );
