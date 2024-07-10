c4949f5620ed52c2e757f0ce2bca5df339145df8
arifwp <arifwahyu696@gmail.com>
2024-07-10 10:22:33 +0700

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/independent/Navbar";
 import "./index.css";
 import Landing from "./pages/Landing";
-import Navbar from "./components/independent/Navbar";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
