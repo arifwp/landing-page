9700c965c61f5d85e57a2f35532a0ada5c8574e1
arifwp <arifwahyu696@gmail.com>
2024-07-10 10:22:40 +0700

    change data

--- src/data/event-list.ts
+++ src/data/event-list.ts
@@ -1,93 +1,136 @@
 interface ArtistItem {
-    id: string,
-    artistName: string,
-    artistFrom: string
+  id: string;
+  artistName: string;
+  artistFrom: string;
 }
 
 export interface EventItem {
-    id: string,
-    name: string,
-    location: string,
-    description: string,
-    time: string,
-    thumbnail: string,
-    artist: Array<ArtistItem>
-  }
+  id: string;
+  name: string;
+  location: string;
+  description: string;
+  time: string;
+  tag: string;
+  thumbnail: string;
+  artist: Array<ArtistItem>;
+}
 
-export const eventList : Array<EventItem> = [
-    {
-        id: 'EV-001',
-        name: 'Karnaval',
-        location: 'Lapangan Desa Ketaon',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-        time: '13.00 - 17.00',
-        thumbnail: '/images/event/2018/karnaval-topeng-ireng.jpg',
-        artist: [
-            {
-                id: 'AR-001',
-                artistName: 'Eva Blezynsky',
-                artistFrom: 'Surakarta'
-            },
-            {
-                id: 'AR-002',
-                artistName: 'Rondo Parwito',
-                artistFrom: 'Purwokerto'
-            },
-        ],
-    },
-    {
-        id: 'EV-002',
-        name: 'Kesenian Anak',
-        location: 'Bundaran HI',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-        time: '19.00 - 22.00',
-        thumbnail: '/images/event/2022/tari-boyolali.jpg',
-        artist: [
-            {
-                id: 'AR-001',
-                artistName: 'Eva Blezynsky',
-                artistFrom: 'Surakarta'
-            },
-            {
-                id: 'AR-002',
-                artistName: 'Rondo Parwito',
-                artistFrom: 'Purwokerto'
-            },
-            {
-                id: 'AR-003',
-                artistName: 'Sigit Purnomo',
-                artistFrom: 'Boyolali'
-            },
-        ],
-    },
-    {
-        id: 'EV-003',
-        name: 'Dangdut & Pop Festival',
-        location: 'Lapangan Desa Ketaon',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-        time: '20.00 - 24.00',
-        thumbnail: '/images/event/2023/biduan-boyolali.jpg',
-        artist: [
-            {
-                id: 'AR-001',
-                artistName: 'Eva Blezynsky',
-                artistFrom: 'Surakarta'
-            },
-        ],
-    },
-    {
-        id: 'EV-004',
-        name: 'Wayang Orang',
-        location: 'Depan Toko Darman',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-        time: '20.00 - 24.00',
-        thumbnail: '/images/event/2023/ketoprak-ketaon-fair.jpg',
-        artist: [
-            {
-                id: 'AR-001',
-                artistName: 'Eva Blezynsky',
-                artistFrom: 'Surakarta'
-            },
-        ],
-    },
+export const eventList: Array<EventItem> = [
+  {
+    id: "EV-001",
+    name: "Karnaval",
+    location: "Lapangan Desa Ketaon",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    time: "13.00 - 17.00",
+    thumbnail: "/images/event/2022/karnaval-kostum-merah-boyolali.jpg",
+    tag: "Festival",
+    artist: [
+      {
+        id: "AR-001",
+        artistName: "Eva Blezynsky",
+        artistFrom: "Surakarta",
+      },
+      {
+        id: "AR-002",
+        artistName: "Rondo Parwito",
+        artistFrom: "Purwokerto",
+      },
+    ],
+  },
+  {
+    id: "EV-002",
+    name: "Pentas Kesenian Anak",
+    location: "Jln. Ketaon",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    time: "18.30 - 22.00",
+    thumbnail: "/images/event/2022/tari-boyolali.jpg",
+    tag: "Festival & Musik",
+    artist: [
+      {
+        id: "AR-001",
+        artistName: "Eva Blezynsky",
+        artistFrom: "Surakarta",
+      },
+      {
+        id: "AR-002",
+        artistName: "Rondo Parwito",
+        artistFrom: "Purwokerto",
+      },
+      {
+        id: "AR-003",
+        artistName: "Sigit Purnomo",
+        artistFrom: "Boyolali",
+      },
+    ],
+  },
+  {
+    id: "EV-003",
+    name: "Panggung Dangdut",
+    location: "Lapangan Desa Ketaon",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    time: "20.00 - 24.00",
+    thumbnail: "/images/event/2023/biduan-boyolali.jpg",
+    tag: "Musik",
+    artist: [
+      {
+        id: "AR-001",
+        artistName: "Eva Blezynsky",
+        artistFrom: "Surakarta",
+      },
+    ],
+  },
+  {
+    id: "EV-004",
+    name: "Kesenian Ketoprak",
+    location: "Jln. Ketaon, Depan Toko Darman",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    time: "20.00 - 24.00",
+    thumbnail: "/images/event/2023/ketoprak-ketaon-fair.jpg",
+    tag: "Festival & Musik",
+    artist: [
+      {
+        id: "AR-001",
+        artistName: "Eva Blezynsky",
+        artistFrom: "Surakarta",
+      },
+    ],
+  },
+  {
+    id: "EV-005",
+    name: "Koes Plus",
+    location: "Halaman Balai Desa Ketaon",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    time: "20.00 - 24.00",
+    thumbnail: "/images/event/2023/ks-plus.jpg",
+    tag: "Musik",
+    artist: [
+      {
+        id: "AR-001",
+        artistName: "Eva Blezynsky",
+        artistFrom: "Surakarta",
+      },
+    ],
+  },
+  {
+    id: "EV-006",
+    name: "Bazar & Culinary",
+    location: "Sepanjang Jalan Ketaon",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    time: "09.00 - 24.00",
+    thumbnail: "/images/event/2022/bazar-culinary.jpg",
+    tag: "Musik",
+    artist: [
+      {
+        id: "AR-001",
+        artistName: "Eva Blezynsky",
+        artistFrom: "Surakarta",
+      },
+    ],
+  },
 ];
--- src/data/event.ts
+++ src/data/event.ts
@@ -1,43 +1,44 @@
 interface Event {
-    eventName: string,
-    eventNameWithYear: string,
-    eventLocation: string,
-    eventBaseCampLocation: string,
-    eventLogo: string,
-    eventTag: string,
-    eventDescription: string, 
-    eventDate: string,
-    videoUrl: string,
-    generalAdminWhatsapp: string,
-    generalAdminName: string,
-    bazarAdminWhatsapp: string,
-    bazarAdminName: string,
-    instagramLink: string,
-    youtubeLink: string,
-    tiktokLink: string,
-    totalMediaPartner: string,
-    totalVisitor: string,
-    totalSponsor: string,
+  eventName: string;
+  eventNameWithYear: string;
+  eventLocation: string;
+  eventBaseCampLocation: string;
+  eventLogo: string;
+  eventTag: string;
+  eventDescription: string;
+  eventDate: string;
+  videoUrl: string;
+  generalAdminWhatsapp: string;
+  generalAdminName: string;
+  bazarAdminWhatsapp: string;
+  bazarAdminName: string;
+  instagramLink: string;
+  youtubeLink: string;
+  tiktokLink: string;
+  totalMediaPartner: string;
+  totalVisitor: string;
+  totalSponsor: string;
 }
 
-export const event : Event = {
-    eventName: 'Ketaon Fair',
-    eventNameWithYear: "Ketaon Fair 2024",
-    eventLocation: 'Ketaon, Banyudono, Boyolali',
-    eventBaseCampLocation: 'Depan Tugu KB Desa Ketaon',
-    eventLogo: '/images/sponsors/coffe.png',
-    eventTag: 'Music, Festival',
-    eventDescription: 'Ketaon Fair meruapakan event tahunan yang diselenggarakan oleh Karang Taruna Manggala Wijaya. Event ini adalah event yang paling ditunggu-tunggu oleh warga sekitar. Dalam event ini sering kali terdapat beberapa acara seperti karnaval yang diikuti oleh semua warga Ketaon serta beberapa hiburan lainnya seperti wayang, dangdut, band pop punk, serta pentas seni anak.',
-    eventDate: 'comingsoon',
-    videoUrl: 'https://www.youtube.com/embed/QhBnZ6NPOY0',
-    generalAdminWhatsapp: '+6281111111111', // phone number must be start with +(country code)
-    generalAdminName: 'Risa',
-    bazarAdminWhatsapp: '+6281111111111',
-    bazarAdminName: 'Yuda',
-    instagramLink: 'https://www.instagram.com/ketaon_fair/',
-    youtubeLink: 'https://www.youtube.com/@manggalawijayaketaon2320',
-    tiktokLink: 'https://www.tiktok.com/@ketaonfair',
-    totalSponsor: '15',
-    totalVisitor: '1500++',
-    totalMediaPartner: '3'
+export const event: Event = {
+  eventName: "Ketaon Fair",
+  eventNameWithYear: "Ketaon Fair 2024",
+  eventLocation: "Ketaon, Banyudono, Boyolali",
+  eventBaseCampLocation: "Depan Balai Desa Ketaon",
+  eventLogo: "/images/sponsors/coffe.png",
+  eventTag: "Music, Festival",
+  eventDescription:
+    "Ketaon Fair meruapakan event tahunan yang diselenggarakan oleh Karang Taruna Manggala Wijaya. Event ini adalah event yang paling ditunggu-tunggu oleh warga sekitar. Dalam event ini sering kali terdapat beberapa acara seperti karnaval yang diikuti oleh semua warga Ketaon serta beberapa hiburan lainnya seperti wayang, dangdut, band pop punk, serta pentas seni anak.",
+  eventDate: "comingsoon",
+  videoUrl: "https://www.youtube.com/embed/QhBnZ6NPOY0",
+  generalAdminWhatsapp: "+6285157060174", // phone number must be start with +(country code)
+  generalAdminName: "Arif",
+  bazarAdminWhatsapp: "+6285157060174",
+  bazarAdminName: "Arif",
+  instagramLink: "https://www.instagram.com/ketaon_fair/",
+  youtubeLink: "https://www.youtube.com/@manggalawijayaketaon2320",
+  tiktokLink: "https://www.tiktok.com/@ketaonfair",
+  totalSponsor: "3",
+  totalVisitor: "1000++",
+  totalMediaPartner: "3",
 };
